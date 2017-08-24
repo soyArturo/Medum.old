d29c3cba8a321f9bb44ae976f338c50b1fc95e3b
Arturo <prz.arturo@gmail.com>
2017-08-23 21:43:10 -0700

    Ya publica en Firebase, falta que las muestre en la app

---
+++ app/src/main/java/com/medum/medum/view/PrincipalActivity.java
@@ -0,0 +1,15 @@
+package com.medum.medum.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.medum.medum.R;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
